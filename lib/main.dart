c1bd205f2186cf575a6690b07fc70fc939f4cd07
David Toledo <dtoledoz0207@gmail.com>
2020-10-01 16:51:16 -0500

    Initial-commit
    LoginPage and HomePage created.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FormValidations',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage()
+      },
+    );
+  }
+}
---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text('Login page')),
+      ),
+    );
+  }
+}
