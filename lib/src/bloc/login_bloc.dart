1d8ca9fe7be5b107934ba29331b55f9e1e5da0b3
David Toledo <dtoledoz0207@gmail.com>
2020-10-05 16:54:51 -0500

    create-loginBloc-to-control-email-and-password-fields

---
+++ lib/src/bloc/login_bloc.dart
@@ -0,0 +1,24 @@
+
+
+import 'dart:async';
+
+class LoginBloc {
+  
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+
+  Stream<String> get emailStream => _emailController.stream;
+  Stream<String> get passwordStream => _passwordController.stream;
+
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+
+}
+
