5236e415b74dbf67f1109c5b6dea6c3018444a64
David Toledo <dtoledoz0207@gmail.com>
2020-10-11 12:38:33 -0500

    create-productModel

---
+++ lib/src/models/product_model.dart
@@ -0,0 +1,42 @@
+// To parse this JSON data, do
+//
+//     final productModel = productModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ProductModel productModelFromJson(String str) => ProductModel.fromJson(json.decode(str));
+
+String productModelToJson(ProductModel data) => json.encode(data.toJson());
+
+class ProductModel {
+
+  String id;
+  String title;
+  double value;
+  bool available;
+  String photoUrl;
+
+  ProductModel({
+      this.id,
+      this.title = '',
+      this.value = 0.0,
+      this.available = true,
+      this.photoUrl,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+      id: json["id"],
+      title: json["title"],
+      value: json["value"].toDouble(),
+      available: json["available"],
+      photoUrl: json["photoUrl"],
+  );
+
+  Map<String, dynamic> toJson() => {
+      "id": id,
+      "title": title,
+      "value": value,
+      "available": available,
+      "photoUrl": photoUrl,
+  };
+}
